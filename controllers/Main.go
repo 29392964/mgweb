f5a59efe3292073949c17607805562205f95b209
yuecaili <yuecaili@yuecailideMacBook-Pro.local>
2016-08-21 07:33:09 +0800

    增加prepare方法

--- controllers/Main.go
+++ controllers/Main.go
@@ -8,23 +8,28 @@ import (
 
 type MainController struct {
 	beego.Controller
+        uri string
+        db string
 }
 
+func (this *MainController) Prepare() {
+    if this.GetSession("uri") == nil || this.GetSession("db") == nil {
+        this.Redirect("/login", 302)
+    }
+    this.uri,this.db = this.GetSession("uri").(string),this.GetSession("db").(string)
+}
+
+
 func (this *MainController) Get() {
-        if this.GetSession("uri") == nil || this.GetSession("db") == nil {
-            this.Redirect("/login", 302) 
-        }
-        uri,db := this.GetSession("uri").(string),this.GetSession("db").(string)
-        this.Data["db"], this.Data["Collections"] = db, models.Collections(uri,db)
+        this.Data["db"], this.Data["Collections"] = this.db, models.Collections(this.uri,this.db)
 	this.TplName = "index.tpl"
 }
 
 func (this *MainController) Post(){
-        uri,db := this.GetSession("uri").(string),this.GetSession("db").(string)
         collection := this.Input().Get("c")
         page, _ := strconv.Atoi(this.Input().Get("page")) 
         pagesize,_ := beego.AppConfig.Int("pagesize")
-        this.Data["Datas"],this.Data["Datas1"],this.Data["Count"],this.Data["PageCount"] = models.Datas(uri,db,collection,page,pagesize)
-        this.Data["Db"],this.Data["Collection"] = db, collection
+        this.Data["Datas"],this.Data["Datas1"],this.Data["Count"],this.Data["PageCount"] = models.Datas(this.uri,this.db,collection,page,pagesize)
+        this.Data["Db"],this.Data["Collection"] = this.db, collection
         this.TplName = "layout/table.tpl"
 }
